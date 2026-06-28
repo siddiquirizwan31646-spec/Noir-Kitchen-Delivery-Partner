81efdf52388e4a942bd7c29acfa88831af1b4bca
siddiquirizwan31646-spec <siddiquirizwan31646@gmail.com>
2026-06-28 20:51:47 +0530

    fix mongoose saslprep issue

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+process.env.SUPPRESS_NO_CONFIG_WARNING = true;
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth");
@@ -28,7 +29,9 @@ app.use("/api/auth", authRouter);
 app.use("/api/delivery-auth", deliveryAuthRouter);
 app.use("/api/assign-orders", assignOrderRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 5000,
+})
   .then(() => {
     app.listen(process.env.PORT || 5000, () =>
       console.log(`Server running on port ${process.env.PORT}`)
